04a56875c3778f335ab2e1440372b161747f97db
Tom Rice <t.rice90@gmail.com>
2014-06-11 02:07:52 -0400

    Added support for the WCSaxes to Galactic viewer

--- astrodendro/viewer.py
+++ astrodendro/viewer.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import numpy as np
 from .plot import DendrogramPlotter
 
+from wcsaxes import WCSAxes
 
 class SelectionHub(object):
 
@@ -54,7 +55,7 @@ class SelectionHub(object):
 
 
 class BasicDendrogramViewer(object):
-    def __init__(self, dendrogram, galactic=False):
+    def __init__(self, dendrogram, galactic=False, wcs=None):
 
         if galactic:
             self.ax1_limits = [0.1, 0.5, 0.8, 0.4] # image
@@ -94,7 +95,12 @@ class BasicDendrogramViewer(object):
         import matplotlib.pyplot as plt
         self.fig = plt.figure(figsize=(14, 8))
 
-        self.ax1 = self.fig.add_axes(self.ax1_limits)
+        if wcs is not None:
+            ax1 = WCSAxes(self.fig, self.ax1_limits, wcs=wcs)
+            self.ax1 = self.fig.add_axes(ax1)
+        else:
+            self.ax1 = self.fig.add_axes(self.ax1_limits)
+
 
         from matplotlib.widgets import Slider
 
